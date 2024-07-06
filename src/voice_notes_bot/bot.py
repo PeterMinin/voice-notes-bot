42d4a76a362edb51baeaf99832b03e4953a147c1
Peter Minin <peterminin@gmail.com>
2024-07-07 01:46:20 +0200

    Handle "done" reactions

--- src/voice_notes_bot/bot.py
+++ src/voice_notes_bot/bot.py
@@ -3,7 +3,8 @@ import asyncio
 import json
 from pathlib import Path
 
-from telegram import Bot, Update
+import telegram as tg
+from telegram.constants import ReactionEmoji
 
 from .audiofile import get_as_ogg_opus
 from .config import Config
@@ -30,25 +31,66 @@ def get_api_token(bot_json: Path) -> str:
     return api_token
 
 
-async def handle_update(update: Update, bot: Bot, config: Config) -> bool:
-    if update.message.chat.id != config.chat_id:
-        print("New chat:", update)
+def is_done_reaction(reaction_updated: tg.MessageReactionUpdated) -> bool:
+    for reaction in reaction_updated.new_reaction:
+        if isinstance(reaction, tg.ReactionTypeEmoji) and reaction.emoji in [
+            ReactionEmoji.THUMBS_UP,
+            ReactionEmoji.OK_HAND_SIGN,
+        ]:
+            return True
+    return False
+
+
+async def handle_reaction(
+    reaction: tg.MessageReactionUpdated, config: Config, state: State
+) -> bool:
+    message_id = reaction.message_id
+    if not is_done_reaction(reaction):
+        print(
+            f"Info: Ignoring reaction {reaction.new_reaction} for message {message_id}"
+        )
         return True
-    if update.message.text == "/start":
-        await bot.send_message(text="Hi!", chat_id=config.chat_id)
+    source_filename = state.message_id_to_filename[str(message_id)]
+    if source_filename is None:
+        print(f"Info: Ignoring a repeated Done reaction for message {message_id}")
         return True
+    file = config.recordings_dir / source_filename
+    if file.is_file():
+        file.unlink()
+    else:
+        print(f"Info: Note source {source_filename} already deleted")
+    ok = await reaction.chat.set_message_reaction(message_id, ReactionEmoji.HANDSHAKE)
+    assert ok
+    state.message_id_to_filename[str(message_id)] = None
+    return True
+
+
+async def handle_update(update: tg.Update, config: Config, state: State) -> bool:
+    if update.message:
+        if update.message.chat.id != config.chat_id:
+            print("New chat:", update)
+            return True
+        if update.message.text == "/start":
+            bot = update.get_bot()
+            await bot.send_message(text="Hi!", chat_id=config.chat_id)
+            return True
+    if update.message_reaction:
+        return await handle_reaction(update.message_reaction, config, state)
     print("Unexpected update:", update)
     return False
 
 
-async def process_updates(bot: Bot, config: Config, state: State):
+async def process_updates(bot: tg.Bot, config: Config, state: State):
     last_update_id = state.last_update_id
-    updates = await bot.get_updates(offset=last_update_id + 1)
+    updates = await bot.get_updates(
+        offset=last_update_id + 1,
+        allowed_updates=[tg.Update.MESSAGE, tg.Update.MESSAGE_REACTION],
+    )
     if not updates:
         print("No updates")
         return
     for i, update in enumerate(updates):
-        if await handle_update(update, bot, config):
+        if await handle_update(update, config, state):
             state.last_update_id = update.update_id
         else:
             print(f"Updates left unprocessed: {len(updates) - i}")
@@ -56,34 +98,34 @@ async def process_updates(bot: Bot, config: Config, state: State):
     print("All updates processed")
 
 
-async def send_voice_note(bot: Bot, chat_id: int, audio_file: Path):
+async def send_voice_note(bot: tg.Bot, chat_id: int, audio_file: Path) -> tg.Message:
     assert audio_file.is_file()
     with get_as_ogg_opus(audio_file) as ogg_file:
-        await bot.send_voice(chat_id, ogg_file, caption=audio_file.stem)
+        return await bot.send_voice(chat_id, ogg_file, caption=audio_file.stem)
 
 
 async def process_voice_notes(
-    bot: Bot, chat_id: int, recordings_dir: Path, state: State
+    bot: tg.Bot, chat_id: int, recordings_dir: Path, state: State
 ):
-    old_files = state.old_files
-
     audio_files = sorted(recordings_dir.glob("*.m4a"), key=lambda file: file.name)
-    sent_files = []
+    filenames = [file.name for file in audio_files]
+    num_sent = 0
     for file in audio_files:
-        if file.name in old_files:
+        if file.name in state.old_files:
             continue
-        await send_voice_note(bot, chat_id, file)
-        sent_files.append(file.name)
+        message = await send_voice_note(bot, chat_id, file)
+        state.message_id_to_filename[message.id] = file.name
+        num_sent += 1
 
-    if sent_files:
-        state.old_files = old_files + sent_files
-        print(f"Notes sent: {len(sent_files)}")
+    state.old_files = filenames
+    if num_sent > 0:
+        print(f"Notes sent: {num_sent}")
     else:
         print("No new notes")
 
 
 async def run_bot(token: str, config: Config, state: State):
-    bot = Bot(token)
+    bot = tg.Bot(token)
     async with bot:
         await process_updates(bot, config, state)
         if config.chat_id is None:
--- src/voice_notes_bot/state.py
+++ src/voice_notes_bot/state.py
@@ -10,6 +10,7 @@ class State:
     _state_dir: Path
     last_update_id: int
     old_files: list[str]
+    message_id_to_filename: dict[str, str]
 
     @classmethod
     def load(cls, state_dir: Path | str) -> "State":
@@ -25,12 +26,14 @@ class State:
             _state_dir=state_dir,
             last_update_id=data.get("last_update_id", 0),
             old_files=data.get("old_files", []),
+            message_id_to_filename=data.get("message_id_to_filename", {})
         )
 
     def save(self) -> None:
         data = {
             "last_update_id": self.last_update_id,
             "old_files": self.old_files,
+            "message_id_to_filename": self.message_id_to_filename,
         }
         with open(self._state_dir / _state_filename, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent="\t")
